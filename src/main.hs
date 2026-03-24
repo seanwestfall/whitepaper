79282fb93fc2de4775f71593177a1be68f64827e
seanwestfall <sean.westfall@gmail.com>
2026-03-23 17:13:16 -0700

    add the inital files

---
+++ src/main.hs
@@ -0,0 +1,4 @@
+module Main (main) where
+
+main :: IO ()
+main = putStrLn "whitepaper: A whitepaper template library in haskell that includes templates for academic, research / policy, consulting, and proposals, and automatic translations."
